9c6e6042569f80a766408ab2b853d8da62e5b4cc
Carlos Edinson Roso Espinosa <ce.roso398@gmail.com>
2018-03-18 00:49:01 -0500

    feat: proof of concept, console.log rule working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import Linter, { FILE_NAME } from "../app/shared/Linter";
+import * as JSON from "circular-json";
+import { RuleFailure, LintResult } from "tslint";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  onKeyup(text) {
+    console.log(text);
+
+    localStorage.setItem("source", text);
+    const _Linter = new Linter({ fix: false });
+    _Linter.lint("TSLintPlayground.ts", localStorage.getItem("source"));
+    const failures = this.getFailures(_Linter.getResult());
+    console.log(failures);
+  }
+
+  getFailures(result: LintResult): string {
+    return result.failures.length === 0
+        ? "(No errors)"
+        : JSON.stringify(result.failures[0].getFailure());
+  }
 }
+
+//  https://github.com/palantir/tslint/issues/1001#issuecomment-353804158
